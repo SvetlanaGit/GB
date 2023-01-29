53e81a277f420bf0614f99534314bae702b7fd5c
SvetlanaGit <sber190888@gmail.com>
2023-01-29 16:44:26 +0300

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Света")
+{
+    Console.WriteLine("Ура, это же Света!");
+}
+else;
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
