b528ffd9ae0d31c52fb3680c6f66e08acefc4cc1
SvetlanaGit <sber190888@gmail.com>
2023-02-01 13:20:47 +0300

    Task 5

---
+++ Task_5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на входе принимает трехзначное число,а на выходе выдает последнюю цифру.
+
+Console.WriteLine("--> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a<1000) && (a>99))
+{
+    int b = a%10;
+    System.Console.WriteLine(b);
+}
+else
+{
+    System.Console.WriteLine("Щшибка!");
+}
