2396d6b2fa52b994d9ecb6690b39f36992b62fca
SvetlanaGit <sber190888@gmail.com>
2023-02-07 10:56:40 +0300

    Задача 1 дз2

--- Task_8_HW/Program.cs
+++ Task_8_HW/Program.cs
@@ -8,11 +8,9 @@
 
 Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-if (a % 2) == 0; 
 {
 System.Console.WriteLine("Да");
-};
-else
+}
 {
     System.Console.WriteLine("Нет");
 }
