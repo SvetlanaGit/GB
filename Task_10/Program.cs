ba15c93de29f73f0f0c1e2d0bde1d3bafa7a9c55
SvetlanaGit <sber190888@gmail.com>
2023-02-09 01:27:04 +0300

    Task 10 (семинар 2)

---
+++ Task_10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа.
+
+
+
+// int rnd = newRandom();
+// int a = rnd.Next(10,100):
+
+int Max (int a, int b)
+{
+    if (a>b) return a;
+    else return b;
+}
+
+Random rnd = new Random();
+int a = rnd.Next(10,100);
+System.Console.WriteLine($"Random = {a}");
+
+int firstNumber = a/10;
+int secondNumber = a%10;
+
+int max = Max(firstNumber, secondNumber);
+System.Console.WriteLine($"Max = {max}");
+
