20d9e582fa6d04dc4d5e3ec380b4afb291503238
SvetlanaGit <sber190888@gmail.com>
2023-02-09 01:37:48 +0300

    Task 11 (семинар 2)

---
+++ Task_11/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+// 456 --> 46
+// 782 --> 72
+// 918 -- 98
+
+
+
+Random rnd = new Random();
+
+int a = rnd.Next(100,1000);
+
+System.Console.WriteLine(a);
+
+int firstNumber = a/100;
+int lastNumber = a%10;
+
+a = firstNumber*10+lastNumber;
+
+System.Console.WriteLine(a);
+
