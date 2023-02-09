550b249601e1b3e4dbd79319b92bb3f8d0ff2c80
SvetlanaGit <sber190888@gmail.com>
2023-02-09 15:24:05 +0300

    Task 12 (семинар 2)

---
+++ Task_12/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая будет принимать на вход два числа, и выводить является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 --> не кратно
+// 16, 4 --> кратно.
+
+
+double newNumber()
+{
+    System.Console.WriteLine("--> ");
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+// double first=newNumber();
+// double second=newNumber();
+
+// if (first%second==0){
+//     System.Console.WriteLine(first + "кратно" + second);
+// }
+// else 
+// System.Console.WriteLine("Некратно, остаток: " + (first%second));
+double num = newNumber();
+if  ((num%7==0) && (num%23==0)) {
+    System.Console.WriteLine(num + "кратно 7 и 23");
+}
+else
+{
+    System.Console.WriteLine(num + "некратно");
+}
+
+
