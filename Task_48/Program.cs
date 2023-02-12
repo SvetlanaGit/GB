8776ee34a4354e5aad242eb440adef9a676209f7
SvetlanaGit <sber190888@gmail.com>
2023-02-12 18:38:29 +0300

    Task 48 (семинар 7)

---
+++ Task_48/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив. Размер m и n, каждый элемент в массиве находится по формуле: Amn = m+n.
+// выведите полученный массив на экран: m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+
+
+
+int[,] arr = new int[3,4];
+Random rnd = new Random();
+
+System.Console.WriteLine(arr.Length);
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      arr[i,j] = rnd.Next(100);  
+    }
+}
+
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write(arr[i,j] + " ");
+    }
+    System.Console.WriteLine();
+}
