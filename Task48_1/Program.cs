7021bc166594a8d0b867e0d45b43a01931f6c1e7
SvetlanaGit <sber190888@gmail.com>
2023-02-12 21:51:51 +0300

    Task 481 (семинар 7)

---
+++ Task48_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// // Задайте двумерный массив. Размер m и n, каждый элемент в массиве находится по формуле: Amn = m+n.
+// выведите полученный массив на экран: m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+
+
+Console.WriteLine("Введите количество строк: --> ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: --> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = new int [m,n];
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array [i,j] = i +j;
+//     }
+// }
+
+int[,] array = new int [m,n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array [i,j] = i +j;
+        Console.Write(array[i,j]);
+    }
+    System.Console.WriteLine();
+}
+
+
+
